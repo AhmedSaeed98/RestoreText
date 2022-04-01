8ef3e5908a74d6a061cc5e6291c84a8962fdff5f
Ahmed Saeed <40553471+AhmedSaeed98@users.noreply.github.com>
2022-04-01 14:38:43 +0200

    Update restoreText.py

--- restoreText.py
+++ restoreText.py
@@ -3,7 +3,7 @@
 
 
 """
-    is valid function to validate if input parameter is a valid key or not
+    isValid a function gets a text and validate wether it is found in our keys or not
 """
 def isValid(text):
     keys = ['i','like','books','in','the','shop']
@@ -12,17 +12,25 @@ def isValid(text):
     else:
         return 0
 
+"""
+    restore is the main function that finds all possible keys and the valid key the desired output string
+"""
 def restore(text):
     output = ""
     next = 0
     for i in range(len(text)):
+        #move to the next keyword directly
         if i < next:
             continue
+        #define a list to hold a tuple of all found possible keys
         list = [('',0)]
+
         #get all possible keys from current index
         for j in range(i+1,len(text)+1):
             if isValid(text[i:j]) and text[i:j] not in list:
                 list.append((text[i:j],str(j)))
+
+        #add the last and longest found key starting from indext i to the main desired output text
         output = output + list[-1][0] + " "
         next = int(list[-1][1])
                 
